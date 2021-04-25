337c8a70073b3fcc3eba8753033da6368c898a5c
billybutcher <34742984+billybutcher@users.noreply.github.com>
2021-04-26 06:24:35 +0800

    Concatenate error???
    Traceback (most recent call last):
      File "/content/neuralTextSegmentation/code/lstm_try_with_highSegLenTrain.py", line 468, in <module>
        model = lstm_model(-1, EMBEDDING_DIM, embedding_W, len(dictionary_object.word2id_dic)) #fix 
      File "/content/neuralTextSegmentation/code/lstm_try_with_highSegLenTrain.py", line 126, in lstm_model
        encoded_info = Merge()([encode_left_drop, encode_mid_drop, encode_right_drop])
      File "/usr/local/lib/python2.7/dist-packages/keras/backend/tensorflow_backend.py", line 75, in symbolic_fn_wrapper
        return func(*args, **kwargs)
      File "/usr/local/lib/python2.7/dist-packages/keras/engine/base_layer.py", line 463, in __call__
        self.build(unpack_singleton(input_shapes))
      File "/usr/local/lib/python2.7/dist-packages/keras/layers/merge.py", line 362, in build
        'Got inputs shapes: %s' % (input_shape))
    ValueError: A `Concatenate` layer requires inputs with matching shapes except for the concat axis. Got inputs shapes: [(None, 11, 1000), (None, 300), (None, 11, 1000)]

--- code/lstm_try_with_highSegLenTrain.py
+++ code/lstm_try_with_highSegLenTrain.py
@@ -124,7 +124,7 @@ def lstm_model(sequences_length_for_training, embedding_dim, embedding_matrix, v
     encode_right = AttentionWithContext(name='encode-right-attention')(context_encoder(context_encoder_intermediate1(convoluted_right)))
     encode_left_drop, encode_mid_drop, encode_right_drop = Dropout(0.2)(encode_left), Dropout(0.2)(encode_mid), Dropout(0.2)(encode_right)
 
-    encoded_info = Merge()([encode_left_drop, encode_mid_drop, encode_right_drop])
+    encoded_info = Merge()([encode_left_drop, encode_mid_drop, encode_right_drop]) #fix: Got inputs shapes: [(None, 11, 1000), (None, 300), (None, 11, 1000)]
 
     decoded = Dense(500, name='decoded')(encoded_info)
     decoded_drop = Dropout(0.25, name='decoded_drop')(decoded)
